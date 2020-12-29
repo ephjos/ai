1d00c2986fabfa899d5bb05c9ebd35bdaa96f163
Joseph Hines <jthines3@gmail.com>
2020-12-29 11:09:22 -0500

    Added install script, can now control snake with rules enforced

--- snake/snakegame.py
+++ snake/snakegame.py
@@ -1,33 +1,45 @@
 #!/usr/bin/env python
 import enum
 import pygame
-import random
+from queue import Queue
+from random import randint
 
 class Color():
-    Black = (0,0,0)
-    Grey = (127,127,127)
-    White = (255,255,255)
-    Red = (200,0,0)
-    Green = (0,200,0)
-    Blue = (0,0,200)
+    Black    = (0,   0,   0)
+    Grey     = (127, 127, 127)
+    White    = (255, 255, 255)
+    Red      = (200, 0,   0)
+    Green    = (0,   200, 0)
+    Blue     = (0,   0,   200)
 
 class Tile(enum.Enum):
-    Empty = enum.auto()
-    Snake = enum.auto()
-    Food = enum.auto()
+    Empty    = enum.auto()
+    Snake    = enum.auto()
+    Food     = enum.auto()
+
+class Direction():
+    Up       = ( 0,-1)
+    Down     = ( 0, 1)
+    Left     = (-1, 0)
+    Right    = ( 1, 0)
+
+class GameOver(enum.Enum):
+    Win      = enum.auto()
+    Loss     = enum.auto()
+    Quit     = enum.auto()
 
 def tile_color(tile):
     d = {
-        Tile.Empty: Color.Grey,
-        Tile.Snake: Color.Blue,
-        Tile.Food: Color.Red,
+        Tile.Empty:    Color.Grey,
+        Tile.Snake:    Color.Blue,
+        Tile.Food:     Color.Red,
     }
     return d[tile]
 
 class SnakeGame:
     def __init__(self,
-                 width=800, height=400, fps=60,
-                 board_size=15, tile_size=None):
+                 width=1000, height=500, fps=10,
+                 board_size=25, tile_size=None):
         self.width = width
         self.height = height
         self.fps = fps
@@ -37,50 +49,98 @@ class SnakeGame:
         self.gap = 1
 
     def draw_tiles(self, surface, tiles):
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                pygame.draw.rect(
-                    surface, tile_color(tiles[i][j]),
-                    pygame.Rect(
-                        i*self.tile_size, (j*self.tile_size)+self.offset,
-                        self.tile_size-self.gap, self.tile_size-self.gap))
+        for (i,j), v in tiles.items():
+            pygame.draw.rect(
+                surface, tile_color(v),
+                pygame.Rect(
+                    i*self.tile_size, (j*self.tile_size)+self.offset,
+                    self.tile_size-self.gap, self.tile_size-self.gap))
+
+    def rand_point(self, tiles):
+        empty_points = [k for k,v in tiles.items() if v == Tile.Empty]
+        l = len(empty_points)-1
+        return empty_points[randint(0,l)]
 
     def play(self):
         # Init pygame
-        pygame.display.init()
+        pygame.display.init() # only need display
         pygame.display.set_caption("SnakeGame")
-        clock = pygame.time.Clock()
+        clock = pygame.time.Clock() # initialize clock for enforcing fps
 
         screen = pygame.display.set_mode((self.width, self.height))
         surface = pygame.Surface((self.width, self.height))
 
-        running = True
-
         # Init game objects
-        default_tiles = [[Tile.Empty for _ in range(self.board_size)]
-                 for _ in range(self.board_size)]
-        food = (int(self.board_size*0.75), self.board_size//2)
+        tiles = {(i,j): Tile.Empty
+                 for i in range(self.board_size)
+                 for j in range(self.board_size)}
         y,x = int(self.board_size*0.25), self.board_size//2
-        snake = [(y,x),(y-1,x),(y-2,x)]
+        food = (int(self.board_size*0.75), x)
+        snake_queue = Queue()
+        for part in [(y-2,x),(y-1,x),(y,x)]:
+            snake_queue.put(part)
+            tiles[part] = Tile.Snake
+        tiles[food] = Tile.Food
+        head = (y,x)
+        direction = Direction.Right
+        game_over = None
+        got_food = False
 
-        while running:
-            tiles = default_tiles.copy()
-            tiles[food[0]][food[1]] = Tile.Food
-            for ty,tx in snake:
-                tiles[ty][tx] = Tile.Snake
+        while not game_over:
+            surface.fill(Color.Black) # clear screen
 
-            surface.fill(Color.Black)
+            # draw current state
             self.draw_tiles(surface, tiles)
             screen.blit(surface, (0,0))
             pygame.display.flip()
 
-
+            # handle events
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    game_over = GameOver.Quit
+                    break
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_UP:
+                        direction = Direction.Up
+                    if event.key == pygame.K_DOWN:
+                        direction = Direction.Down
+                    if event.key == pygame.K_LEFT:
+                        direction = Direction.Left
+                    if event.key == pygame.K_RIGHT:
+                        direction = Direction.Right
 
+            # enforce fps and screen update
             clock.tick(self.fps)
             pygame.display.update()
+
+            # game state updates
+            next_head = (head[0]+direction[0],head[1]+direction[1])
+
+            # Enforce board boundaries
+            if not 0 <= next_head[0] < self.board_size or \
+               not 0 <= next_head[1] < self.board_size:
+                game_over = GameOver.Loss
+                # TODO: Handle score
+                break
+
+            # Detect tail collision
+            if tiles[next_head] == Tile.Snake:
+                game_over = GameOver.Loss
+                # TODO: Handle score
+                break
+
+            # Detect picking up food
+            if tiles[next_head] == Tile.Food:
+                got_food = True
+                tiles[self.rand_point(tiles)] = Tile.Food
+                # TODO: Handle score
+
+            snake_queue.put(next_head)
+            tiles[next_head] = Tile.Snake
+            head = next_head
+            if not got_food:
+                tiles[snake_queue.get_nowait()] = Tile.Empty
+            got_food = False
 
         pygame.quit()
 
